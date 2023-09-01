783375e725eba4705c5da29af59e6bae78c0eea2
rezas <rez.salehipour@gmail.com>
2023-09-01 13:46:22 +0100

    create a diffrent widget for gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,33 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        // backgroundColor: Colors.amber,
-        // backgroundColor: Color.fromARGB(255, 63, 5, 120),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world!',
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        ),
-      ),
+          // backgroundColor: Colors.amber,
+          // backgroundColor: Color.fromARGB(255, 63, 5, 120),
+          body: GradientContainer()),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
