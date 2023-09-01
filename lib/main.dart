2c369d944ec0f1972af59979a9b5503e0646c2e0
rezas <rez.salehipour@gmail.com>
2023-09-01 13:45:02 +0100

    create a gradient background view

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Colors.amber,
+        // backgroundColor: Color.fromARGB(255, 63, 5, 120),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello world!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
